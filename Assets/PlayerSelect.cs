b9b77be87a93be835ea757faf96ad8b3d659c01d
Evan Czyzycki <eczy3826@gmail.com>
2018-03-18 00:28:36 -0400

    tweaks'

--- Assets/PlayerSelect.cs
+++ Assets/PlayerSelect.cs
@@ -12,10 +12,12 @@ public class PlayerSelect : MonoBehaviour {
 	public bool auto_select = false;
 	[Header("Order: red pos, red neg, blue pos, blue neg")]
 	public Player[] players;
-	public Text start_text;
+	public Text press_start_text;
+	public float text_switch_delay = 1f;
 
 	bool[] locked;
 	int[] selections;
+	bool finished = false;
 
 	// Use this for initialization
 	void Start () {
@@ -41,11 +43,13 @@ public class PlayerSelect : MonoBehaviour {
 			}
 			Finish ();
 		}
-		start_text.enabled = false;
+		press_start_text.gameObject.SetActive(false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (finished)
+			return;
 		for (int i = 0; i < InputManager.Devices.Count; i++) {
 			InputDevice d = InputManager.Devices [i];
 			if (locked [i]) {
@@ -57,23 +61,25 @@ public class PlayerSelect : MonoBehaviour {
 			
 			player_buttons [i].localPosition = new Vector3 (d.LeftStickX, d.LeftStickY, 0).normalized * radius;
 			if (d.Action1.WasPressed && d.LeftStick.Vector.magnitude > 0) {
-				if (locked [i])
-					continue;
 				player_buttons [i].localPosition = new Vector3 (Mathf.Sign (d.LeftStickX), Mathf.Sign (d.LeftStickY), 0).normalized * radius;
 				locked [i] = true;
 
 				// Top Right
-				if (Mathf.Sign (d.LeftStickX) > 0 && Mathf.Sign (d.LeftStickY) > 0)
+				if (Mathf.Sign (d.LeftStickX) > 0 && Mathf.Sign (d.LeftStickY) > 0) {
 					selections [i] = 2;
+				}
 				// Top Left
-				else if (Mathf.Sign (d.LeftStickX) < 0 && Mathf.Sign (d.LeftStickY) > 0)
+				else if (Mathf.Sign (d.LeftStickX) < 0 && Mathf.Sign (d.LeftStickY) > 0) {
 					selections [i] = 0;
+				}
 				// Bottom Right
-				else if (Mathf.Sign (d.LeftStickX) > 0 && Mathf.Sign (d.LeftStickY) < 0)
+				else if (Mathf.Sign (d.LeftStickX) > 0 && Mathf.Sign (d.LeftStickY) < 0) {
 					selections [i] = 3;
+				}
 				// Bottom Left
-				else if (Mathf.Sign (d.LeftStickX) < 0 && Mathf.Sign (d.LeftStickY) < 0)
+				else if (Mathf.Sign (d.LeftStickX) < 0 && Mathf.Sign (d.LeftStickY) < 0) {
 					selections [i] = 1;
+				}
 			}
 		}
 
@@ -82,20 +88,27 @@ public class PlayerSelect : MonoBehaviour {
 			if (locked [i] == false)
 				return;
 		}
-		start_text.enabled = true;
+		press_start_text.gameObject.SetActive(true);
 		if (InputManager.ActiveDevice.MenuWasPressed)
-			Finish ();
+			StartCoroutine(Finish ());
 	}
 
-	void Finish(){
-		start_text.gameObject.SetActive (false);
+	IEnumerator Finish(){
 		foreach (RectTransform r in player_buttons)
 			r.gameObject.SetActive (false);
 		foreach (RectTransform r in player_select_areas)
 			r.gameObject.SetActive (false);
 
+		press_start_text.text = "READY";
+		yield return new WaitForSeconds (text_switch_delay);
+		press_start_text.color = Color.red;
+		press_start_text.text = "GO";
+
 		for (int i = 0; i < players.Length; i++) {
 			players [selections[i]].player_number = i;
 		}
+
+		yield return new WaitForSeconds (text_switch_delay);
+		press_start_text.gameObject.SetActive (false);
 	}
 }
