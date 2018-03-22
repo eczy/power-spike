85c61667cc8ecbdc80988c3f91f3a73524bec103
NickKoester <nick.koester5@gmail.com>
2018-03-22 18:08:15 -0400

    Standalone tutorial one player

---
+++ Assets/TutorialManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour {
+
+    PlayerTutorial[] tutorials;
+
+    bool ready = false;
+
+	void Start () {
+        tutorials = FindObjectsOfType<PlayerTutorial>();
+	}
+    
+	void Update () {
+        if (!ready && CheckPlayersReady())
+        {
+            Debug.Log("Ready");
+        }
+	}
+
+    bool CheckPlayersReady()
+    {
+        foreach (PlayerTutorial player in tutorials)
+        {
+            if (!player.IsReady())
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
