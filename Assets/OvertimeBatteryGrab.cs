9b2faa289ccdc774653c1e361b6bb40ef36747ae
Andrew Sharon <apsharon@umich.edu>
2018-03-23 13:05:13 -0400

    fixed error

---
+++ Assets/OvertimeBatteryGrab.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OvertimeBatteryGrab : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        BatteryCollector collector = other.GetComponent<BatteryCollector>();
+
+//        if (collector && collector.CanGrab() && !CollectorOnThisTeam(collector))
+ //       {
+  //          Battery battery = Instantiate(batteryPrefab).GetComponent<Battery>();
+   //         if (battery)
+    //        {
+     //           collector.TakeBattery(battery);
+      //      }
+       // }
+    }
+}
