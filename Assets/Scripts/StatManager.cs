a8d466ca1745aac5fcaa95f40d33212af327f5b6
Mark Koester <mark.koester1@gmail.com>
2018-03-26 13:24:39 -0400

    Fixed stat bugs

--- Assets/Scripts/StatManager.cs
+++ Assets/Scripts/StatManager.cs
@@ -3,6 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class StatManager : MonoBehaviour {
+    private void Awake()
+    {
+        StatManager[] stats = FindObjectsOfType<StatManager>();
+
+        foreach (StatManager stat in stats)
+        {
+            if (gameObject != stat.gameObject)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
     private void Start()
     {
         DontDestroyOnLoad(gameObject);
