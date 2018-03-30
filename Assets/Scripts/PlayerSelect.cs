d8d8dd30dba9863e41063864afdd8c4b9d1c4ed1
Mark Koester <mark.koester1@gmail.com>
2018-03-29 20:47:05 -0400

    Player select lerp and color change on select

--- Assets/Scripts/PlayerSelect.cs
+++ Assets/Scripts/PlayerSelect.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 using InControl;
@@ -7,155 +8,199 @@ using InControl;
 public class PlayerSelect : MonoBehaviour {
 
 	public float radius = 1f;
-	public RectTransform[] player_buttons;
-	public RectTransform[] player_select_areas;
-	public bool auto_select = false;
+	public RectTransform[] playerButtons;
+	public RectTransform[] playerSelectAreas;
+	public bool autoSelect = false;
 	[Header("Order: red pos, red neg, blue pos, blue neg")]
 	public Player[] players;
-	public Text press_start_text;
-	public float text_switch_delay = 1f;
+	public Text pressStartText;
+	public float textSwitchDelay = 1f;
+    public Color selectedColor;
 
-    PlayerAssignment assignments;
-	bool[] locked;
-	int[] selections;
-	bool finished = false;
+    private PlayerAssignment assignments;
+    private bool[] locked;
+    private int[] selections;
+    private bool selectionDone;
+    private Color originalColor;
 
-	// Use this for initialization
-	void Start () {
-
-		for (int i = 0; i < players.Length; i++) {
-			players [i].player_number = 100;
+    private void Start()
+    {
+		foreach (Player player in players)
+		{
+		    player.player_number = -1;
 		}
 
 		locked = new bool[players.Length];
-		for (int i = 0; i < locked.Length; i++) {
-			locked [i] = false;
-		}
 
 		selections = new int[players.Length];
 		for (int i = 0; i < selections.Length; i++) {
-			selections [i] = 100;
+			selections[i] = -1;
 		}
 
-		if (auto_select) {
+		if (autoSelect) {
 			for (int i = 0; i < selections.Length; i++) {
 				selections [i] = i;
 			}
-			Finish ();
+			StartCoroutine(Finish());
 		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (finished)
-			return;
-		
-		for (int i = 0; i < InputManager.Devices.Count; i++) {
-			InputDevice d = InputManager.Devices [i];
-			if (locked [i]) {
-				if (d.Action2.WasPressed) {
-					locked [i] = false;
-					selections [i] = 100;
-				}
-				else
-					continue;
-			}
-			
-			player_buttons [i].localPosition = new Vector3 (d.LeftStickX, d.LeftStickY, 0).normalized * radius;
-			if (d.Action1.WasPressed && d.LeftStick.Vector.magnitude > 0) {
-				player_buttons [i].localPosition = new Vector3 (Mathf.Sign (d.LeftStickX), Mathf.Sign (d.LeftStickY), 0).normalized * radius;
 
-				// Top Right
-				if (Mathf.Sign (d.LeftStickX) > 0 && Mathf.Sign (d.LeftStickY) > 0) {
-					bool already_exists = false;
-					for (int j = 0; j < selections.Length; ++j)
-						if (selections [j] == 2)
-							already_exists = true;
-					if (already_exists)
-						continue;
-					selections [i] = 2;
-					locked [i] = true;
-				}
-				// Top Left
-				else if (Mathf.Sign (d.LeftStickX) < 0 && Mathf.Sign (d.LeftStickY) > 0) {
-					bool already_exists = false;
-					for (int j = 0; j < selections.Length; ++j)
-						if (selections [j] == 0)
-							already_exists = true;
-					if (already_exists)
-						continue;
-					selections [i] = 0;
-					locked [i] = true;
-				}
-				// Bottom Right
-				else if (Mathf.Sign (d.LeftStickX) > 0 && Mathf.Sign (d.LeftStickY) < 0) {
-					bool already_exists = false;
-					for (int j = 0; j < selections.Length; ++j)
-						if (selections [j] == 3)
-							already_exists = true;
-					if (already_exists)
-						continue;
-					selections [i] = 3;
-					locked [i] = true;
-				}
-				// Bottom Left
-				else if (Mathf.Sign (d.LeftStickX) < 0 && Mathf.Sign (d.LeftStickY) < 0) {
-					bool already_exists = false;
-					for (int j = 0; j < selections.Length; ++j)
-						if (selections [j] == 1)
-							already_exists = true;
-					if (already_exists)
-						continue;
-					selections [i] = 1;
-					locked [i] = true;
-				}
-			}
-		}
-        int selected = 0;
-        for (int i = 0; i < selections.Length; i++)
+    private void Update()
+    {
+        if (!selectionDone)
         {
-            if (selections[i] != 100)
-                selected++;
+            UpdateSelections();
         }
-        if (selected < 1)
-            return;
-
-		if (InputManager.ActiveDevice.MenuWasPressed)
-			StartCoroutine(Finish ());
 	}
 
-	IEnumerator Finish(){
-		finished = true;
-		foreach (RectTransform r in player_buttons)
-			r.gameObject.SetActive (false);
-		foreach (RectTransform r in player_select_areas)
-			r.gameObject.SetActive (false);
+    private void UpdateSelections()
+    {
+		for (int i = 0; i < InputManager.Devices.Count; i++) {
+            UpdateDevice(i);
+        }
 
-        press_start_text.transform.position = Camera.main.WorldToScreenPoint(Vector3.zero);
-		press_start_text.text = "READY";
-		yield return new WaitForSeconds (text_switch_delay);
-		press_start_text.color = Color.red;
-		press_start_text.text = "GO";
+        // Stop player selection when a device presses start with at least one player selected
+        bool selectionMade = selections.Count(t => t != -1) > 0;
+        if (selectionMade && InputManager.ActiveDevice.MenuWasPressed)
+        {
+			StartCoroutine(Finish());
+        }
+    }
+
+    private void UpdateDevice(int deviceIndex)
+    {
+        InputDevice device = InputManager.Devices[deviceIndex];
+
+        bool selectionCancelled = locked[deviceIndex] && device.Action2.WasPressed;
+
+        if (selectionCancelled) {
+            UnlockPlayer(deviceIndex);
+        }
+
+        if (locked[deviceIndex]) return;
+
+        bool deviceMadeSelection = device.Action1.WasPressed && device.LeftStick.Vector.magnitude > 0;
+
+        if (deviceMadeSelection)
+        {
+            LockPlayer(deviceIndex);
+        }
+        else
+        {
+            playerButtons[deviceIndex].localPosition = new Vector3(device.LeftStickX, device.LeftStickY, 0) * radius;
+        }
+
+    }
+
+    private IEnumerator Finish(){
+		selectionDone = true;
+
+        foreach (RectTransform r in playerButtons)
+        {
+			r.gameObject.SetActive (false);
+        }
+
+        foreach (RectTransform r in playerSelectAreas)
+        {
+			r.gameObject.SetActive (false);
+        }
+
+        pressStartText.transform.position = Camera.main.WorldToScreenPoint(Vector3.zero);
+		pressStartText.text = "READY";
+
+		yield return new WaitForSeconds (textSwitchDelay);
+
+		pressStartText.color = Color.red;
+		pressStartText.text = "GO";
 
 		for (int i = 0; i < players.Length; i++) {
-            if (selections[i] != 100)
+		    if (selections[i] != -1)
+		    {
 			    players [selections[i]].player_number = i;
+		    }
 		}
 
         assignments = FindObjectOfType<PlayerAssignment>();
         foreach (Player player in players)
         {
-            if (player.player_number != 100)
+            if (player.player_number == -1) continue;
+
+            Charge charge = player.GetComponent<Charge>();
+            assignments.StorePlayerAssignment(player.team, charge.charge, player.player_number);
+            player.gameObject.SetActive(true);
+        }
+
+		yield return new WaitForSeconds (textSwitchDelay);
+
+		pressStartText.gameObject.SetActive (false);
+        FindObjectOfType<TutorialManager>().enabled = true;
+	}
+
+    private void LockPlayer(int deviceIndex)
+    {
+        Vector3 direction = playerButtons[deviceIndex].localPosition.normalized;
+
+        int quadrant = GetQuadrant(direction);
+
+        if (PlayerSelected(quadrant)) return;
+
+		playerButtons[deviceIndex].localPosition = new Vector3(Mathf.Sign(direction.x), Mathf.Sign(direction.y), 0).normalized * radius;
+        selections[deviceIndex] = quadrant;
+        locked[deviceIndex] = true;
+
+        Image buttonImage = playerButtons[deviceIndex].GetComponent<Image>();
+        originalColor = buttonImage.color;
+        buttonImage.color = selectedColor;
+    }
+
+    private void UnlockPlayer(int deviceIndex)
+    {
+        locked[deviceIndex] = false;
+        selections[deviceIndex] = -1;
+
+        playerButtons[deviceIndex].GetComponent<Image>().color = originalColor;
+    }
+
+    private bool PlayerSelected(int player)
+    {
+        bool selected = false;
+        foreach (int selection in selections)
+        {
+            if (selection == player)
             {
-                Charge charge = player.GetComponent<Charge>();
-                assignments.StorePlayerAssignment(player.team, charge.charge, player.player_number);
-                player.gameObject.SetActive(true);
+                selected = true;
             }
         }
 
-		yield return new WaitForSeconds (text_switch_delay);
-		press_start_text.gameObject.SetActive (false);
+        return selected;
+    }
 
-        FindObjectOfType<TutorialManager>().enabled = true;
-	}
+    private int GetQuadrant(Vector3 direction)
+    {
+
+        int quadrant = -1;
+
+        // Top Right
+        if (direction.x > 0 && direction.y > 0)
+        {
+            quadrant = 2;
+        }
+        // Top Left
+        else if (direction.x < 0 && direction.y > 0)
+        {
+            quadrant =  0;
+        }
+        // Bottom Right
+        else if (direction.x > 0 && direction.y < 0)
+        {
+            quadrant = 3;
+        }
+        // Bottom Left
+        else if (direction.x < 0 && direction.y < 0)
+        {
+            quadrant = 1;
+        }
+
+        return quadrant;
+    }
 }
