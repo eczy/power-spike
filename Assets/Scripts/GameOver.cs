3150e6a2aea0671509fc0881b7929a8a8229467b
NickKoester <nick.koester5@gmail.com>
2018-04-02 10:42:49 -0400

    Overtime now only happens on tie

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -4,13 +4,25 @@ public class GameOver : MonoBehaviour {
 	public string statScene;
 	public BatteryGoal redTeam;
 	public BatteryGoal blueTeam;
-	
+
+    Timer timer;
+    Overtime overtime;
+
+    private void Start()
+    {
+        timer = FindObjectOfType<Timer>();
+    }
+
     private void Update () {
-		if (redTeam.GetBatteries () == redTeam.maxBatteries) {
+		if (redTeam.GetBatteries() == redTeam.maxBatteries) {
 			EndGame(Team.Red);
-		} else if (blueTeam.GetBatteries () == blueTeam.maxBatteries) {
+		} else if (blueTeam.GetBatteries() == blueTeam.maxBatteries) {
 			EndGame(Team.Blue);
 		}
+        else if (timer && timer.IsTimeOut())
+        {
+            CalculateTimeoutWinner();
+        }
 	}
 	
     private void EndGame(Team winningTeam)
@@ -18,5 +30,23 @@ public class GameOver : MonoBehaviour {
 	    StatManager.SetWinningTeam(winningTeam);
 	    SceneTransitionController.RequestSceneTransition(statScene, 1f);
     }
+
+    void CalculateTimeoutWinner()
+    {
+        if (redTeam.GetBatteries() > blueTeam.GetBatteries())
+        {
+            EndGame(Team.Red);
+        }
+        else if (blueTeam.GetBatteries() > redTeam.GetBatteries())
+        {
+            EndGame(Team.Blue);
+        }
+        else
+        {
+            overtime.StartOvertime();
+        }
+    }
 	
+
+
 }
