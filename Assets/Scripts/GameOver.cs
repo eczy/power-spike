b902379655db05074680ac7f240cfb78f296a3c6
Evan Czyzycki <eczy3826@gmail.com>
2018-04-15 22:34:23 -0400

    new game over anim

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -1,12 +1,19 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class GameOver : MonoBehaviour {
 	public string statScene;
 	public BatteryGoal redTeam;
 	public BatteryGoal blueTeam;
+    public float loserDeathDelay = 1f;
+    public float loserThrowForce = 1f;
+    public float loserThrowTorque = 1f;
+    public Rigidbody[] redTeamPlayers;
+    public Rigidbody[] blueTeamPlayers;
 
     Timer timer;
     Overtime overtime;
+    bool gameOver = false;
 
     private void Start()
     {
@@ -15,10 +22,12 @@ public class GameOver : MonoBehaviour {
     }
 
     private void Update () {
+        if (gameOver)
+            return;
 		if (redTeam.GetBatteries() == redTeam.maxBatteries) {
-			EndGame(Team.Red);
+			StartCoroutine(EndGame(Team.Red));
 		} else if (blueTeam.GetBatteries() == blueTeam.maxBatteries) {
-			EndGame(Team.Blue);
+			StartCoroutine(EndGame(Team.Blue));
 		}
         else if (timer && timer.IsTimeOut())
         {
@@ -26,25 +35,54 @@ public class GameOver : MonoBehaviour {
         }
 	}
 	
-    private void EndGame(Team winningTeam)
+    private IEnumerator EndGame(Team winningTeam)
     {
+        gameOver = true;
 	    StatManager.SetWinningTeam(winningTeam);
+        if (winningTeam == Team.Red)
+            KillLosers(Team.Blue);
+        else
+            KillLosers(Team.Red);
+        yield return new WaitForSeconds(loserDeathDelay);
 	    SceneTransitionController.RequestSceneTransition(statScene, 1f);
+        yield return null;
     }
 
     void CalculateTimeoutWinner()
     {
         if (redTeam.GetBatteries() > blueTeam.GetBatteries())
         {
-            EndGame(Team.Red);
+            StartCoroutine(EndGame(Team.Red));
         }
         else if (blueTeam.GetBatteries() > redTeam.GetBatteries())
         {
-            EndGame(Team.Blue);
+            StartCoroutine(EndGame(Team.Blue));
         }
         else
         {
             overtime.StartOvertime();
+        }
+    }
+
+    void KillLosers(Team team)
+    {
+        Rigidbody[] losers = null;
+        if (team == Team.Red)
+        {
+            Debug.Log("killing red team");
+            losers = redTeamPlayers;
+        }
+        else
+        {
+            print("killing blue team");
+            losers = blueTeamPlayers;
+        }
+
+        foreach (Rigidbody r in losers)
+        {
+            r.constraints = RigidbodyConstraints.None;
+            r.AddForce(new Vector3(0, 0, Random.Range(-loserThrowForce, loserThrowForce)), ForceMode.Impulse);
+            r.AddTorque(new Vector3(0, Random.Range(-loserThrowTorque, loserThrowTorque), 0), ForceMode.Impulse);
         }
     }
 	
